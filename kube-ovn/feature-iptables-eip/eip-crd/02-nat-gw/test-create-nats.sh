12d199832e8319fe014afbbea479053dba19c902
zhangbingbing <zhangbingbing@yealink.com>
2022-03-14 03:40:18 +0000

    go on

--- kube-ovn/feature-iptables-eip/eip-crd/02-nat-gw/test-create-nats.sh
+++ kube-ovn/feature-iptables-eip/eip-crd/02-nat-gw/test-create-nats.sh
@@ -8,6 +8,7 @@ kubectl apply -f vipd02.yaml
 kubectl apply -f vips01.yaml
 kubectl apply -f vipf01.yaml
 kubectl apply -f vipf02.yaml
+kubectl apply -f vip-no-subnet-01.yaml
 
 sleep 2
 
@@ -18,6 +19,7 @@ kubectl apply -f 00-eipf01.yml
 kubectl apply -f 00-eipf02.yml
 kubectl apply -f 00-eipd01.yml 
 kubectl apply -f 00-eipd02.yml
+kubectl apply -f 00-eip-no-subnet-01.yaml
 
 sleep 2
 
@@ -26,6 +28,7 @@ sleep 2
 kubectl apply -f 01-snat.yml 
 kubectl apply -f 02-fip-rule01.yaml
 kubectl apply -f 02-fip-rule02.yaml
+kubectl apply -f 02-fip-no-subnet-01.yaml
 kubectl apply -f 03-dnat01.yaml
 kubectl apply -f 03-dnat02.yaml
 
--- kube-ovn/feature-iptables-eip/eip-crd/02-nat-gw/test-delete-nats.sh
+++ kube-ovn/feature-iptables-eip/eip-crd/02-nat-gw/test-delete-nats.sh
@@ -7,6 +7,8 @@ kubectl delete -f 02-fip-rule01.yaml
 kubectl delete -f 02-fip-rule02.yaml
 kubectl delete -f 03-dnat01.yaml
 kubectl delete -f 03-dnat02.yaml
+kubectl delete -f 02-fip-no-subnet-01.yaml
+
 
 
 # clean eip
@@ -16,6 +18,8 @@ kubectl delete -f 00-eipf01.yml
 kubectl delete -f 00-eipf02.yml
 kubectl delete -f 00-eipd01.yml 
 kubectl delete -f 00-eipd02.yml
+kubectl delete -f 00-eip-no-subnet-01.yaml
+
 
 sleep 2
 
@@ -26,6 +30,7 @@ kubectl delete -f vipd02.yaml
 kubectl delete -f vips01.yaml
 kubectl delete -f vipf01.yaml
 kubectl delete -f vipf02.yaml
+kubectl delete -f vip-no-subnet-01.yaml
 
 sleep 2
 
