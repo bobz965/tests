85a1a77b2378ed9fdcdc2e652712cc65dec7725c
bobz965 <1220582684@qq.com>
2022-06-17 18:02:51 +0800

    调整环境到集群外部

--- ci/kube-ovn-self-ci/link.sh
+++ ci/kube-ovn-self-ci/link.sh
@@ -10,3 +10,6 @@ ln -s kube-ovn-self-ci/02-l1-kube-ovn-base-dockerfile 02-l1-kube-ovn-base-docker
 ln -s kube-ovn-self-ci/03-l1-kube-ovn-controller-dockerfile 03-l1-kube-ovn-controller-dockerfile
 ln -s kube-ovn-self-ci/04-vpc-nat-gateway-docker-file 04-vpc-nat-gateway-docker-file
 ln -s kube-ovn-self-ci/release.rc release.rc
+
+
+rm -f 001-build-ubuntu-base.sh 002-build-l1-kube-ovn-base.sh 003-build-l1-kube-ovn-controller.sh 004-build-vpc-nat-gateway.sh 01-ubuntu-base-docker-file 02-l1-kube-ovn-base-dockerfile 03-l1-kube-ovn-controller-dockerfile 04-vpc-nat-gateway-docker-file release.rc
