c3cbe1f91f9fb7adf29dec4757d4154756874192
bobz965 <1220582684@qq.com>
2022-07-22 14:41:47 +0800

    up

---
+++ ci/kube-ovn-self-ci/005-build-l1-kube-ovn-daemon.sh
@@ -0,0 +1,29 @@
+source ./release.rc
+name=l1-kube-ovn-daemon
+image=$name
+echo "ready to build base image: $image:$release"
+docker build --network=host -t $image:$release -f 05-l1-kube-ovn-daemon-dockerfile .
+
+url=registry.yealinkops.com/third_party/kolla
+
+line=`docker images | grep $image | grep $release`
+
+
+num=`echo $line | wc -l`
+if [ "$num" != "1" ];then
+      echo "匹配到的镜像数目不是1"
+      exit 0
+fi
+with_nsname=`echo $line |awk -F' ' '{print $1}'`	
+tag=`echo $line |awk -F' ' '{print $2}'`
+tag_image="$with_nsname:$tag"
+registry_tag="$url/$image:$release"
+echo "tag $tag_image --> $registry_tag ..."
+docker tag $tag_image   $registry_tag
+echo "ready to push $registry_tag ..."
+docker push $registry_tag
+
+# clean image
+
+# docker image prune  -a -f
+# docker rmi $(docker images -f "dangling=true" -q) -f
--- ci/kube-ovn-self-ci/link.sh
+++ ci/kube-ovn-self-ci/link.sh
@@ -4,11 +4,13 @@
 #ln -s kube-ovn-self-ci/001-build-ubuntu-base.sh 001-build-ubuntu-base.sh
 ln -s kube-ovn-self-ci/002-build-l1-kube-ovn-base.sh 002-build-l1-kube-ovn-base.sh
 ln -s kube-ovn-self-ci/003-build-l1-kube-ovn-controller.sh 003-build-l1-kube-ovn-controller.sh
+ln -s kube-ovn-self-ci/005-build-l1-kube-ovn-daemon.sh 005-build-l1-kube-ovn-daemon.sh
 ln -s kube-ovn-self-ci/004-build-vpc-nat-gateway.sh 004-build-vpc-nat-gateway.sh
 #ln -s kube-ovn-self-ci/01-ubuntu-base-docker-file 01-ubuntu-base-docker-file
 ln -s kube-ovn-self-ci/02-l1-kube-ovn-base-dockerfile 02-l1-kube-ovn-base-dockerfile
 ln -s kube-ovn-self-ci/03-l1-kube-ovn-controller-dockerfile 03-l1-kube-ovn-controller-dockerfile
 ln -s kube-ovn-self-ci/04-vpc-nat-gateway-docker-file 04-vpc-nat-gateway-docker-file
+ln -s kube-ovn-self-ci/05-l1-kube-ovn-daemon-dockerfile 05-l1-kube-ovn-daemon-dockerfile
 ln -s kube-ovn-self-ci/release.rc release.rc
 
 
